61095cf3a1910a37f9d3415b76f3830519c7bdc6
carloshosiqueira <carlos.siqueira.des@gmail.com>
2023-10-03 11:21:09 -0300

    add

---
+++ LIMA/AULA 09/script.js
@@ -0,0 +1,200 @@
+//let variavel local
+//var variavel global
+//const variavel constante 
+
+// const obj = [
+//     {
+//     name: 'lucas',
+//     age: 20,
+//     country: 'Brazil'
+//     },
+//     {
+//     name: 'Agatha',
+//     age: 12,
+//     country: 'Brazil'
+//     },
+//     {
+//     name: 'Maria',
+//     age: 2,
+//     country: 'Brazil'
+//     }
+// ]
+
+// console.log(obj)
+// console.warn('AVISO')
+// console.error('ERRO')
+// console.table(obj)
+
+//operadores aritimeticos
+//  + soma
+//  - subtração
+//  ( * multiplicação
+//  / divisão
+//  ( ** potenciaçao 
+//  % resto da divisão
+
+//operadores de comparaçao
+// > maior que
+// < menor que
+// >= maior ou igual que
+// <= menor ou igual que
+// = atribuiçao 
+// == igualdade
+// === igualdade (valor e tipo)
+//()/ != diferente
+// 
+//() !==igualdde (valor e tipo)
+
+
+// Exercicio 1 da primeira lista 
+
+//declaraçao de variaveis 
+
+// let a = 5
+// let b = 10
+// let c = 15
+
+//a soma de 'a' e 'b' dividido por c
+
+// console.log((a + b )/ c);
+
+//Exercicio 2 da primeira lista 
+
+
+//declaraçao das variaveis 
+
+// var velocidade = 10202
+// var distancia = 12222
+
+
+//conta para informar o tempo apos tal distancia e velocidade 
+
+// console.log(velocidade/distancia)
+
+
+//Exercicios 3 da primeira lista 
+
+
+//declaraçao de variaveis 
+
+// var salario = 2800
+// var porcentual  = 15
+
+
+// porcentual 
+// var conta = (salario * porcentual) / 100
+
+//conta final
+// var final = ( salario + conta)
+
+// console.log("O seu salário é: " +final+ ", Com o desconto de "+porcentual+"%");
+ 
+// var data = new Date();
+// var dia = data.getDate();
+// var mes = data.getMonth() + 1;
+// var ano = data.getFullYear();
+
+
+// var hora = data.getHours();
+// var min = data.getMinutes();
+
+// var anoComplato = dia + '/' + mes + '/' + ano;
+
+// var anoComplato = `${dia}/${mes}/${ano}`;
+
+// var horaComplata = `${hora}:${min}`;
+
+// console.log(anoCompleto, horaComplata);
+// Math.ceil(); arredonda p cima
+//Math.floor(); arredonda p baixo
+// Math.roudi(); arredonda p numero mais proximo 
+
+// var n = '10'
+
+// Number.parseInt(); converte para inteiro
+
+// 
+
+// if(){
+
+// }else if{
+
+// }else{
+
+//}
+
+//condiçao ? true : false; - if terminario
+// switch(n){
+//     case 1;
+//     break;
+//     default;
+//     break;
+
+// }
+// for(let i = 0; < 10; i++){
+//     console.log(i);
+// }
+
+// while(){
+
+// }
+
+// do(){
+
+// }while{
+
+// }
+
+// var list = 'a b c d e'. split(' ')
+
+// for (let i = 0; i < list.length; i++){
+//     console.log(list[i]);
+// }
+
+// list.forEach(item =>{ 
+//     console.log(item);
+// })
+
+// for (let item of list){
+//       console.log(item)
+// }
+
+// function nome (){
+
+// }
+// nome ();
+
+//1 Desenvolva um programa que leia o preço de um produto e se o preço for maior do que 1000 reais aplique um desconto de 8%. Mostre o preço final.
+
+// var preco = 1001
+
+// if (preco > 1000){
+//     var precoAgr = preco * 0.8 
+//     console.log(precoAgr)
+// }
+
+//2 Desenvolva um programa que leia o salário de um funcionário e o número de filhos.
+//Se o salário for menor do que 2000 o funcionário receberá um salário família equivalente a 45 reais por filho. Apresente o salário final
+
+// var salario
+// var numFilho
+// console.log("Digite seu Salário")
+// salario=Number(prompt())
+// numFilho= Number(prompt());
+// if ((salario>2000 && numFilho>=45)){
+
+
+// }
+
+//1 - Faça um programa que mostre na tela valores no intervalo entre 10 e 200;
+
+// var num
+// for (num=10 ; num <= 200; num++) {
+//     console.log(`${num}`)
+// }
+
+//2 - Faça um programa que mostre na tela valores no intervalo entre 200 e 10;
+// var num
+//  for (num=200 ; num >= 10; num--) {
+//      console.log(`${num}`)
+//  }
