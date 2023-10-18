7d7eb207f9e792d8a95c16d3f10ab76e0a8a1ffd
anyocher <anaochnerort@gmail.com>
2023-10-18 10:53:04 -0300

    18/10/2023

--- FPOO/AULA 16/ATIVIDADE DA AULA 16/Avaliaçao Fisica/Javascript.js
+++ FPOO/AULA 16/ATIVIDADE DA AULA 16/Avaliaçao Fisica/Javascript.js
@@ -8,48 +8,59 @@ class paciente{
     imc(){
         return this.nome = this.peso / (this.altura * this.altura);
     }
-    diagnosticar(){
-        if(this.imc() < 18.5){
-            return "Abaixo do peso";
-        }else if(this.imc() > 18.5 && this.imc() < 24.9){
-            return "Peso normal";
-        }else if(this.imc() > 25 && this.imc() < 29.9){
-            return "Sobrepeso";
-        }else if(this.imc() > 30 && this.imc() < 34.9){
-            return "Obesidade grau 1";
-        }else if(this.imc() > 35 && this.imc() < 39.9){
-            return "Obesidade grau 2";
-        }else {
-            return "Obesidade grau 3";
-        }
+    inss() {
+        if (this.salario < 1320.01)
+            return this.salario * 7.5 / 100;
+        else if (this.salario < 2571.30)
+            return this.salario * 9 / 100;
+        else if (this.salario < 3856.95)
+            return this.salario * 12 / 100;
+        else if (this.salario < 7507.49)
+            return this.salario * 14 / 100;
+        else
+            return 3051.65;
+           
     }
+    irrf() {
+        if (this.salario < 1903.99)
+            return 0;
+        else if (this.salario < 2826.66)
+            return this.salario * 7.5 / 100 - 142.8;
+        else if (this.salario < 3751.06)
+            return this.salario * 15 / 100 - 354.8;
+        else if (this.salario < 4664.69)
+            return this.salario * 22.5 / 100 - 636.13;
+        else
+            return this.salario * 27.5 / 100 - 869.36;
+    }
+
 
 showHTML(){
     let str = `<label></label><label> ${this.nome}</label>`;
     str += `<hr>`
     str += `<div class="espaco">`
-    str += `<label>Peso: </label><label> ${this.peso}</label>`;
+    str += `<label>Sálario </label><label> ${this.peso}</label>`;
     str += `</div>`
     str += `<div class="espaco">`
-    str += `<label>Altura: </label><label> ${this.altura}</label>`;
+    str += `<label>INSS </label><label> ${this.altura}</label>`;
     str += `</div>`
     str += `<div class="espaco">`
-    str += `<label>IMC: </label><label> ${this.imc().toFixed(2)}</label>`;
+    str += `<label>IRRF </label><label> ${this.imc().toFixed(2)}</label>`;
     str += `</div>`
-    str += `<hr>`
-    str += `<label>Diagnosticar: </label><label> ${this.diagnosticar()}</label>`;
+    str += `<label>Sálario Líquido</label><label> ${this.inss()}</label>`;
+   
+    
     return str;
     }
 }
 
 const lista = [];
-lista.push(new paciente(`André Mattos`, 80, 1.8));
-lista.push(new paciente(`João da Silva`, 100, 1.7));
-lista.push(new paciente(`Maria de Souza`, 60, 1.65));
-lista.push(new paciente(`José Oliveira`, 90, 1.75));
-lista.push(new paciente(`Silvia Andrade`, 60, 1.85));
-lista.push(new paciente(`Sueli Oliveira`, 44, 1.65));
-lista.push(new paciente(`Pedro de Souza`, 110, 1.85));
+lista.push(new paciente(`CatNoir `, 8044, 234));
+lista.push(new paciente(`LadyBug`, 1049, 984));
+lista.push(new paciente(`Marinette`,7890, 485));
+lista.push(new paciente(`Adrien`,1890, 15));
+lista.push(new paciente(`Tikki`,4890, 185));
+lista.push(new paciente(`Alya`,2890, 85));
 
 console.table(lista);
 
--- FPOO/AULA 17/Saida com arquivo/JavaScript.js
+++ FPOO/AULA 17/Saida com arquivo/JavaScript.js
@@ -1,50 +1,173 @@
-class salario {
-    constructor(salario, inss){
-
-      this.salario = salario;
-      this.inss = inss;
-      this.irrf = irrf;
-
-}
-
-    irrf(salario) {
-            if (salario < 1903.99)
-               return 0;
-        else if (salario < 2826.66)
-                return salario * 7.5 / 100 - 142.8;
-        else if (salario < 3751.06)
-                 return salario * 15 / 100 - 354.8;
-        else if (salario < 4664.69)
-                return salario * 22.5 / 100 - 636.13;
-        else
-                return salario * 27.5 / 100 - 869.36;
+class Pagamento{
+    constructor(nome, salario){
+        this.nome = nome
+        this.salario = salario
+        this.salarioBase = (this.salario - this.inss()).toFixed(2)
+        this.salarioLiquido = (this.salarioBase - this.irrf()).toFixed(2)
     }
-    inss(salario) {
-            if (salario < 1320.01)
-                 return salario * 7.5 / 100;
-        else if (salario < 2571.30)
-                 return salario * 9 / 100;
-        else if (salario < 3856.95)
-                 return salario * 12 / 100;
-        else if (salario < 7507.49)
-                 return salario * 14 / 100;
-        else
-                 return 1051.05;
-         }
 
-   
+    inss() {
+        if (this.salario < 1320.01)
+            return this.salario * 7.5 / 100;
+        else if (this.salario < 2571.30)
+            return this.salario * 9 / 100;
+        else if (this.salario < 3856.95)
+            return this.salario * 12 / 100;
+        else if (this.salario < 7507.49)
+            return this.salario * 14 / 100;
+        else
+            return 1051.05;
+    }
+
+    irrf() {
+        if (this.salario < 1903.99)
+            return 0;
+        else if (this.salario < 2826.66)
+            return this.salario * 7.5 / 100 - 142.8;
+        else if (this.salario < 3751.06)
+            return this.salario * 15 / 100 - 354.8;
+        else if (this.salario < 4664.69)
+            return this.salario * 22.5 / 100 - 636.13;
+        else
+            return this.salario * 27.5 / 100 - 869.36;
+    }
+    
 }
 
-  console.log(novoSalario.inss);
-let salario = 1000;
-let inss = inss(salario);
-let salarioBase = salario - inss;
-let irrf = irrf(salarioBase);
-let salarioLiquido = salarioBase - irrf;
+const cardgrid = document.querySelector(".card-grid")
+const inputName = document.getElementById("inputName")
+const inputIncome = document.getElementById("inputIncome")
 
+function toTable(obj){
+    let div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML = `
+    <h2 id="objNome">${obj.nome}</h2>
+    <h3 id="objSalario">${obj.salario}</h3>
+    <h3 id="objInss">${obj.inss().toFixed(2)}</h3>
+    <h3 id="objSalBase">${obj.salarioBase}</h3>
+    <h3 id="objIrrf">${obj.irrf().toFixed(2)}</h3>
+    <h3 id="objSalLiquido">${obj.salarioLiquido}</h3>
+    <div class="card-buttons">
+        <button id="editButton">Editar</button>
+        <button id="removeButton">Remover</button>
+    </div>`
+    cardgrid.appendChild(div)
 
-console.log("Salário: " + salario);
-console.log("INSS: " + inss);
-console.log("Salário Base: " + salarioBase);
-console.log("IRRF: " + irrf);
-console.log("Salário Líquido: " + salarioLiquido);
+    div.querySelector('#editButton').addEventListener('click', function(){
+        let editar = document.getElementById('message')
+        let buttons = document.getElementById('buttons')
+        document.getElementById('messageTitle').innerHTML = `Deseja editar o funcionário '${obj.nome}'?`
+
+        let buttonEdit = document.createElement('button')
+        buttonEdit.innerHTML = 'Editar'
+        buttonEdit.setAttribute('id','exclude');
+        buttonEdit.addEventListener('click', function(){
+            document.getElementById('exclude').remove()
+            document.getElementById('cancel').remove()
+            message.style.display = 'none'
+
+            let nameInput = document.createElement('input')
+            let salaryInput = document.createElement('input')
+            salaryInput.setAttribute('type','number');
+
+            let exButtons = div.querySelector('.card-buttons')
+
+            let buttonDone = document.createElement('button')
+            buttonDone.classList.add('button-done')
+            buttonDone.innerHTML = 'Done'
+
+            buttonDone.addEventListener('click', function(){
+                obj.nome = nameInput.value
+                obj.salario = salaryInput.value
+                div.querySelector('#objInss').innerHTML = obj.inss(obj.salario)
+                div.querySelector('#objIrrf').innerHTML = obj.irrf(obj.salario)
+                obj.salarioBase = (salaryInput.value - obj.inss()).toFixed(2)
+                obj.salarioLiquido = (obj.salarioBase - obj.irrf()).toFixed(2)
+
+                console.log(obj.nome)
+                div.querySelector("#objNome").innerHTML = nameInput.value
+                div.querySelector("#objSalario").innerHTML = salaryInput.value
+                div.querySelector("#objSalBase").innerHTML = obj.salarioBase
+                div.querySelector("#objSalLiquido").innerHTML = obj.salarioLiquido
+                nameInput.remove()
+
+                buttonDone.style.display = 'none'
+                exButtons.querySelector('#editButton').style.display = 'block'
+                exButtons.querySelector('#removeButton').style.display = 'block'
+
+                console.log(obj)
+            })
+
+            exButtons.querySelector('#editButton').style.display = 'none'
+            exButtons.querySelector('#removeButton').style.display = 'none'
+            exButtons.appendChild(buttonDone)
+
+            div.querySelector("#objNome").innerHTML = ''
+            div.querySelector("#objSalario").innerHTML = ''
+            div.querySelector("#objNome").appendChild(nameInput)
+            div.querySelector("#objSalario").appendChild(salaryInput)
+            
+        })
+        buttons.appendChild(buttonEdit)
+
+        let buttonCancel = document.createElement('button')
+        buttonCancel.innerHTML = 'Cancelar'
+        buttonCancel.setAttribute('id','cancel');
+        buttonCancel.setAttribute('onclick','excludeCancel()');
+        buttons.appendChild(buttonCancel)
+        editar.style.display = 'flex'
+    })
+
+    div.querySelector('#removeButton').addEventListener('click', function(){
+        let remover = document.getElementById('message')
+        let buttons = document.getElementById('buttons')
+        document.getElementById('messageTitle').innerHTML = `Deseja remover o funcionário '${obj.nome}'?`
+
+        let buttonExclude = document.createElement('button')
+        buttonExclude.innerHTML = 'Remover'
+        buttonExclude.setAttribute('id','exclude');
+        buttonExclude.addEventListener('click', function(){
+            div.remove()
+            document.getElementById('exclude').remove()
+            document.getElementById('cancel').remove()
+            remover.style.display = 'none'
+        })
+        buttons.appendChild(buttonExclude)
+
+        let buttonCancel = document.createElement('button')
+        buttonCancel.innerHTML = 'Cancelar'
+        buttonCancel.setAttribute('id','cancel');
+        buttonCancel.setAttribute('onclick','excludeCancel()');
+        buttons.appendChild(buttonCancel)
+        remover.style.display = 'flex'
+    })
+}
+
+function excludeCancel(){
+    message.style.display = 'none'
+
+    document.getElementById('exclude').remove()
+    document.getElementById('cancel').remove()
+}
+
+function insert(){
+    if(inputName.value == '' && inputIncome.value == ''){
+        inputName.classList.add('alert')
+        inputIncome.classList.add('alert')
+    }else if(inputName.value == ''){
+        inputName.classList.add('alert')
+    }else if(inputIncome.value == ''){
+        inputIncome.classList.add('alert')
+    }else{
+        const pessoa = new Pagamento(inputName.value, Number(inputIncome.value))
+        inputName.value = ''
+        inputIncome.value = ''
+        toTable(pessoa)
+    }
+
+    setTimeout(() => {
+        inputName.classList.remove('alert')
+        inputIncome.classList.remove('alert')
+    }, 1000);
+}
