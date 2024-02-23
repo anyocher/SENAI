f6175cdba9d7c8527c36f4cd7a064534b9e9e590
Landim9 <landimr9des@gmail.com>
2024-02-23 08:25:19 -0300

    autogit

--- 2DES/INDMO/aula 05/sol/App.js
+++ 2DES/INDMO/aula 05/sol/App.js
@@ -1,20 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, Switch,ImageBackground, ScrollView } from 'react-native';
+
+const  icon = require('./assets/icon.png');
+const backgroundImage = require('./assets/fundoceu.png');
 
 export default function App() {
+const [habilitado, setHabilitado] = new useState(false);
+
+const habilitar = () => {
+  setHabilitado(!habilitado);
+}
+
   return (
+    <ImageBackground source={backgroundImage} style={styles.fun}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+      source={icon}
+      style={styles.icon}
+      />
+      <Switch
+      value={habilitado}
+      onValueChange={habilitar}
+      />
+      <Image
+      source={{
+        uri: (habilitado)
+        ? "./assets/sun.png"
+        : "./assets/full-moon.png"
+      }}
+      style={[styles.anime, {opacity: 0.8}]}
+      />
+      <Image
+      source={{
+        // uri: "https://w7.pngwing.com/pngs/924/553/png-transparent-thirty-one-gift-idea-business-organization-plan-man-service-sticker-musician.png"
+
+      }}
+      style={styles.pessoa}
+      />
       <StatusBar style="auto" />
     </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+  },
+  fun: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor:'rgba(255, 255, 255, 0.5)', // Adicionando transparência ao contêiner
     alignItems: 'center',
     justifyContent: 'center',
+    gap: '32px'
   },
+  icon: {
+    width: '68px',
+    height: '68px'
+  },
+  anime: {
+    width: '80px',
+    height: '80px'
+  },
+  pessoa: {
+    width: '200px',
+    height: '200px'
+  }
 });
